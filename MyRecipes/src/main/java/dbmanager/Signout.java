62acc272cd52e378d88acc051c18a2187ee75a4c
Shu Sian Lyu <shusianlyu@gmail.com>
2022-07-26 15:44:35 +0800

    logout & app & home pages added

--- MyRecipes/src/main/java/dbmanager/Signout.java
+++ MyRecipes/src/main/java/dbmanager/Signout.java
@@ -1,6 +1,11 @@
 package dbmanager;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,18 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class logout
- */
-@WebServlet("/logout")
-public class Logout extends HttpServlet {
+@WebServlet("/signout")
+public class Signout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		session.invalidate();
 		response.sendRedirect("login.jsp");
 	}
 
+
 }
+
