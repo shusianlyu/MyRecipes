04cdb1f8726f5735b5bf90566de64bd650e7f2bb
Koosha Kimelman <72764893+Koosha-Kimelman@users.noreply.github.com>
2022-08-02 14:03:56 -0700

    Update Donate_Recipes.java
    Cleaned up unnecessary import statement

--- src/main/java/dbmanager/Donate_Recipes.java
+++ src/main/java/dbmanager/Donate_Recipes.java
@@ -3,7 +3,6 @@ package dbmanager;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
