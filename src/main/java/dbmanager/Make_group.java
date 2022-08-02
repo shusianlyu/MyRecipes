d964c2103d99e1ec8d25e4959fdba7ce6a81ed40
Koosha Kimelman <72764893+Koosha-Kimelman@users.noreply.github.com>
2022-08-02 14:01:38 -0700

    Update Make_group.java
    Cleaned up unnecessary import statements

--- src/main/java/dbmanager/Make_group.java
+++ src/main/java/dbmanager/Make_group.java
@@ -1,9 +1,6 @@
 package dbmanager;
 
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
